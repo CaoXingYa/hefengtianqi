1ba135667d39956746a04bf0cffa6699460ec58f
CXY <924258021@qq.com>
2020-11-13 10:29:04 +0800

    This is my second commit

---
+++ app/src/androidTest/java/com/example/ydxmsj/Album.java
@@ -0,0 +1,19 @@
+package com.example.ydxmsj;
+
+import org.litepal.annotation.Column;
+import org.litepal.crud.LitePalSupport;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Album extends LitePalSupport {
+    @Column(unique = true, defaultValue = "unknown")
+    private String name;
+
+    @Column(index = true)
+    private float price;
+
+    private List<Song> songs = new ArrayList<>();
+
+    // generated getters and setters.
+}
