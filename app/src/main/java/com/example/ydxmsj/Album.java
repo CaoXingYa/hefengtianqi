7664fa3f3db4cff88652e1eeb6ccfea8849dede1
CXY <924258021@qq.com>
2020-11-20 09:15:15 +0800

    This is my second commit

---
+++ app/src/main/java/com/example/ydxmsj/Album.java
@@ -0,0 +1,54 @@
+package com.example.ydxmsj;
+
+import org.litepal.annotation.Column;
+import org.litepal.crud.LitePalSupport;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Album extends LitePalSupport {
+    @Column(unique = true, defaultValue = "unknown")
+    private String name;
+
+    @Column(index = true)
+    private float price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public List<Song> getSongs() {
+        return songs;
+    }
+
+    public void setSongs(List<Song> songs) {
+        this.songs = songs;
+    }
+
+    private Date releaseDate;
+
+    private List<Song> songs = new ArrayList<>();
+
+    // generated getters and setters.
+}
--- app/src/main/java/com/example/ydxmsj/MainActivity.java
+++ app/src/main/java/com/example/ydxmsj/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.ydxmsj;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import org.litepal.tablemanager.Connector;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        //创建数据库表
+        Button createDatabase = (Button) findViewById(R.id.create_database);
+        createDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Connector.getDatabase();
+            }
+        });
+
+        //添加数据
+        Button addData = (Button) findViewById(R.id.add_data);
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Album album = new Album();
+                album.setName("album");
+                album.setPrice(10.99f);
+                album.save();
+                Song song1 = new Song();
+                song1.setName("song1");
+                song1.setDuration(320);
+                song1.setAlbum(album);
+                song1.save();
+                Song song2 = new Song();
+                song2.setName("song2");
+                song2.setDuration(356);
+                song2.setAlbum(album);
+                song2.save();
+
+
+            }
+        });
+}
 }
